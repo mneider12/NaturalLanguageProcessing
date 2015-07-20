51806d31b15bb67661a0742372b44e4c897494b5
Mark Nyder <mneider.virginia@gmail.com>
2015-07-20 16:01:36 -0500

    Initialize with start of Word and agent

---
+++ src/naturalLanguageProcessor/NLPagent.java
@@ -0,0 +1,21 @@
+package naturalLanguageProcessor;
+
+public class NLPagent {
+	
+	private String sentence;
+
+	public NLPagent() {
+		
+	}
+	
+	
+	/*
+	 * Purpose:	Set 
+	 */
+	public void setSentence(String sentence) {
+		
+		this.sentence = sentence;
+		
+	}
+	
+}
